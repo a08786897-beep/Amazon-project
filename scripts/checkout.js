12df5881bdcebab1b84cd1cb1c0b2c7c86e1bcfe
Anshul Arora <a08786897@gamil.com>
2026-07-01 00:46:20 +0530

    when click delete button remove quantity to cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart,removecart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatcuurancy } from "./utils/money.js";
 let cartSummeryHTML='';
@@ -34,7 +34,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -88,3 +88,9 @@ cart.forEach((cartItem) => {
           </div>`;
 });
 document.querySelector('.js-cart-summart').innerHTML=cartSummeryHTML;
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', ()=>{
+    const productId = link.dataset.productId;
+    removecart(productId);
+  });
+});
