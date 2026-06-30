b355661eeeddb5f7dbe9eb7cb04b3692d96e660d
Anshul Arora <a08786897@gamil.com>
2026-07-01 01:10:46 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,17 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
+
+function saveTocart(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addTocart(productId){
     let matchingItem;
@@ -22,6 +29,7 @@ export function addTocart(productId){
           quantity:1
         });
       }
+      saveTocart();
 }
 export function removecart(productId){
   const newCart=[];
@@ -31,4 +39,5 @@ export function removecart(productId){
     }
   });
   cart=newCart;
+  saveTocart();
 }
