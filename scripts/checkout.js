80530f29188057f9220b1b2d7e47681dab83ec39
Anshul Arora <a08786897@gamil.com>
2026-07-01 00:56:59 +0530

    Delete item for html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,7 +11,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummeryHTML +=
-  ` <div class="cart-item-container">
+  ` <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -92,5 +92,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', ()=>{
     const productId = link.dataset.productId;
     removecart(productId);
+    const container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
