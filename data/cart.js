7ecf1b4d3741aec44ba199dff90b4675f401dc08
Anshul Arora <a08786897@gamil.com>
2026-07-04 16:38:03 +0530

    make payment summary interactive

--- data/cart.js
+++ data/cart.js
@@ -1,48 +1,55 @@
-export let cart=JSON.parse(localStorage.getItem('cart'));
-if(!cart){
-  cart=[{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2,
-  delivaryoptionId:'1'
-},{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1,
-  delivaryoptionId:'2'
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
 }
 
- export function saveTocart(){
-  localStorage.setItem('cart',JSON.stringify(cart));
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addTocart(productId){
-    let matchingItem;
-      cart.forEach((cartItem)=>{
-        if(productId===cartItem.productId){
-          matchingItem=cartItem;
-          }
-      });
-      if(matchingItem){
-        matchingItem.quantity++;
-      }else{
+export function addToCart(productId) {
+  let matchingItem;
 
-        cart.push({
-          productId:productId,
-          quantity:1,
-          delivaryoptionID:'2'
-        });
-      }
-      saveTocart();
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
+
+  saveToStorage();
 }
-export function removecart(productId){
-  const newCart=[];
-  cart.forEach((cartItem) =>{
-    if(cartItem.productId !== productId){
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
-  cart=newCart;
-  saveTocart();
+
+  cart = newCart;
+
+  saveToStorage();
 }
 export function updateQuantity(productId, newQuantity) {
   let matchingItem;
@@ -56,4 +63,18 @@ export function updateQuantity(productId, newQuantity) {
   matchingItem.quantity = newQuantity;
 
   saveTocart();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
-export function formatcuurancy(priceCents){
+export function formatCurrency(priceCents){
     return (priceCents/100).toFixed(2);
 }
